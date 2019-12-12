44b13d6208b89ef003044c453b207528fd71edd6
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-12 00:41:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   if array.length == 1 
-    puts array[0]
+    return array[0]
     
   elsif array == 2 
-    puts array.join("and ")
+    return array.join("and ")
 end
 end
